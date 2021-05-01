247ac4d4018eea1c316a9923159efdea9dd46588
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-01 02:54:09 +0200

    Added class App-logo to App.js header. Modified CSS for the logo

--- src/Heading.js
+++ src/Heading.js
@@ -5,8 +5,9 @@ const Heading = () => {
     <header className="App-header">
       CYF Hotel
       <img
+        className="App-logo"
         src="https://image.flaticon.com/icons/svg/139/139899.svg"
-        alt="building image"
+        alt="rotating building"
       />
     </header>
   );
