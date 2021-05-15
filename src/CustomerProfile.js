b97b863a0955b3b8cc9498cecef851828a959405
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-15 11:43:25 +0200

    Lesson 3: Exercise 21 - Fetched customer profile and rendered results in a table. URL link doesn't work so I hard coded a number to complete the rest of the code

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CustomerProfile = props => {
-  const showProfile = () => {
-    console.log("Dog");
-  };
+  const [profile, setProfile] = useState("");
+  console.log("Fetching your customer profile");
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me/customers/3`) //fix the link
+      .then(response => response.json())
+      .then(data => {
+        setProfile(data);
+      });
+  }, [props.id]); //Is this the right dependency?
+
   return (
     <div>
-      <h1>Customer {props.id}'s Profile</h1>
-      <p>{showProfile}</p>
+      <h2>Customer {props.id}'s Profile</h2>
+      <ul>
+        <li>ID: {profile.id}</li>
+        <li>EMAIL: {profile.email}</li>
+        <li>VIP: {profile.vip ? "Yes" : "No"}</li>
+        {/* Boolean? */}
+        <li>PHONE: {profile.phoneNumber}</li>
+      </ul>
     </div>
   );
 };
