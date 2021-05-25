a15026437f8ca272f8df7f27c5348200323ea0ce
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-26 00:19:17 +0200

    Modified Lesson 3, 21 - Table, not list

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -2,25 +2,42 @@ import React, { useEffect, useState } from "react";
 
 const CustomerProfile = props => {
   const [profile, setProfile] = useState("");
-  console.log("Fetching your customer profile");
+  console.log(`Fetching customer ${props.id}'s profile`);
   useEffect(() => {
     fetch(`https://cyf-react.glitch.me/customers/${props.id}`)
       .then(response => response.json())
       .then(data => {
         setProfile(data);
       });
-  }, [props.id]); //Is this the right dependency?
+  }, [props.id]);
 
   return (
     <div>
       <h2>Customer {props.id}'s Profile</h2>
-      <ul>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Email</th>
+            <th scope="col">VIP</th>
+            <th scope="col">Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{profile.id}</td>
+            <td>{profile.email}</td>
+            <td>{profile.vip ? "Yes" : "No"}</td>
+            <td>{profile.phoneNumber}</td>
+          </tr>
+        </tbody>
+      </table>
+      {/* <ul>
         <li>ID: {profile.id}</li>
         <li>EMAIL: {profile.email}</li>
         <li>VIP: {profile.vip ? "Yes" : "No"}</li>
-        {/* Boolean? */}
         <li>PHONE: {profile.phoneNumber}</li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
